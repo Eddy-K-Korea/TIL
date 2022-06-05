872d9774b7acb0e606ffd4680e375314004ab7b4
Byungin Kim <britz0615@gmail.com>
2022-06-05 17:19:03 +0900

    Add files via upload

---
+++ JPA/jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MemberForm {
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+}
---
+++ JPA/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor //final을 가진 변수로 생성자 생성
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /*
+    //테스트나 구동시 치명적인 잘못된 inject을 막기 위해 생성자 주입 사용
+    public MemberService( MemberRepository memberRepository ) {
+        this.memberRepository = memberRepository;
+    }*/
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
+}
